21499e418c3dfb0abe294a6e1a8f03ac38f04f64
lutangkai <1225615644@qq.com>
2023-09-19 22:55:16 +0800

    添加面试笔记

---
+++ Assets/Scripts/TestList.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> ints = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
